b1e31a6e610723de9f2e5aa766b67f008538f804
maisel <maikoltondini@gmail.com>
2025-06-16 00:48:00 +0200

    implemented routes / models in py.

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 #db: setting and initializing
+app.config['SECRET_KEY'] = 'DEV_SECRET_KEY_123'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite'
 app.config["SQLITE_TRACK_MODIFICATIONS"]= False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 #______class / models_____
-#text model
-class Text(db.Model):
+#Post model
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
     creation_date = db.Column(db.DateTime, default=datetime.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     def __repr__(self):
         return f"<Text: {self.id}, title: {self.title}>"
@@ -31,9 +42,114 @@ class Tag(db.Model):
     def __repr__(self):
         return f"<Tag: {self.id} named {self.tag_name}>"
 
+
+#comment model
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    creation_date = db.Column(db.DateTime, default=datetime.now())
+    
+def __repr__(self):
+    return f"<Comment id: {self.id} from {self.author}>"
+    
+
+#user model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(28), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    
+    post = db.relationship('Post', backref='author', lazy='true')
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f'<User: {self.username} email: {self.email}'
+    
+    
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    if request.method=='POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        
+        #basics checks
+        if not username or not email or not password or not confirm_password:
+            flash('All fields are mandatory', 'error')
+            return redirect(url_for('register'))
+        
+        if password != confirm_password:
+            flash('Passwords are not matching, try again', 'error')
+            return redirect(url_for('register'))
+        
+        if User.query.filter_by(username=username).first():
+            flash('This username already exists', 'error')
+            return redirect(url_for('register'))
+        
+        if User.query.filter_by(email=email).first():
+            flash('This email already exists', 'error')
+            return redirect(url_for('register'))
+        
+        new_user = User(username=username, email=email)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('login'))
+    
+    return render_template('register', title="Register")
 
+#login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+    
+        if user and user.check_password(password):
+            login_user(user)
+            flash('Successfully logged in!', 'success')
+        
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('Access deniet. Check email and password', 'error')
+
+    return render_template('login.html', title='login')
+
+      
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been disconnected', 'info')
+    return redirect(url_for('index'))
+
+#PROTECTED ROUTE -DASHBOARD
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', title='dashboard')
